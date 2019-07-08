d1a05fc431ceec0d03afe6edc8c86ebfb0ba25e7
gribar <llandro.g@gmail.com>
2019-07-08 13:12:42 +0300

    lesson 1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -1,6 +1,8 @@
 @file:Suppress("UNUSED_PARAMETER")
 package lesson1.task1
 
+//import kotlin.math.sqrt
+
 import kotlin.math.*
 
 /**
@@ -15,7 +17,9 @@ fun sqr(x: Int) = x * x
  *
  * Вычисление квадрата вещественного числа
  */
-fun sqr(x: Double) = x * x
+fun sqr(x: Double): Double {
+    return x * x
+}
 
 /**
  * Пример
@@ -48,8 +52,9 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
  * Пример главной функции
  */
 fun main(args: Array<String>) {
-    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
-    println("Root product: $x1x2")
+    val x = 7
+    val result = sqr(x)
+    println("$x * $x = $result")
 }
 
 /**
@@ -75,7 +80,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg.toDouble() + min.toDouble()/60 + sec.toDouble()/3600)/180 * PI
 
 /**
  * Тривиальная
@@ -91,7 +96,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = (number - number%100 - (number / 1000)*1000)/100
 
 /**
  * Простая
